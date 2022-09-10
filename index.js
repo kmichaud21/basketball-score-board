08a9866f58e4bd673cb9df9b3f379f44d78dc668
k_michaud <kvinmichaud@yahoo.com>
2022-09-10 00:03:00 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+let countEl1= document.getElementById("count-el-1")
+let countEl2= document.getElementById("count-el-2")
+
+let count = 0
+
+function incrementhome1() {
+    count = count + 1
+    countEl1.innerText = count
+}
+
+function incrementhome2() {
+    count = count + 2
+    countEl1.innerText = count
+}
+
+function incrementhome3() {
+    count = count + 3
+    countEl1.innerText = count
+}
+
+function increment() {
+    count = count + 1
+    countEl2.innerText = count
+}
+
+function increment2() {
+    count = count + 2
+    countEl2.innerText = count
+}
+
+function increment3() {
+    count = count + 3
+    countEl2.innerText = count
+}
